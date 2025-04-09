3e8beecbddcd0c83fe47d1e4162ee67ad511d525
YUNHWAN <629jyh7@naver.com>
2025-04-09 14:18:41 +0900

    add: oauth login 구현 및 리팩토링

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Sidebar.css"; // ✅ 추가
-import { FaHome, FaHistory, FaCog, FaBars } from "react-icons/fa";
+import { FaHome, FaHistory, FaCog, FaBars } from "react-icons/fa"; // ✅ FaGoogle 추가
 
 function CustomSidebar() {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div style={{ height: "100vh" }}>
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <ProSidebar collapsed={collapsed}>
         <Menu iconShape="circle">
           <MenuItem
